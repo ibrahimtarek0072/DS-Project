c8bffa9d2712a8a46bdd3b7cf0c4ad1d28e5d105
ibrahimtarek0072 <81970421+ibrahimtarek0072@users.noreply.github.com>
2021-07-11 23:35:47 +0200

    Update main.cpp

--- DataProject/main.cpp
+++ DataProject/main.cpp
@@ -1,24 +1,96 @@
 #include <iostream>
 
-using namespace std;
+using namespace std ;
+class treap{
+public:
+    treap** node;
+    treap* left;
+    treap*right;
+    string userName;
+    int key;
+};
+
+/*treap* insert(treap* node,int key){
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+
+    node->hight = 1 + max(hight(node->left),
+                           hight(node->right));
+
+    int b = balance(node);
+
+
+
+    if (b > 1 && key < node->left->key)
+        return right_rotate(node);
+
+
+    if (b < -1 && key > node->right->key)
+        return left_rotate(node);
+
+
+    if (b > 1 && key > node->left->key)
+    {
+        node->left = left_rotate(node->left);
+        return right_rotate(node);
+    }
+
+
+    if (b< -1 && key < node->right->key)
+    {
+        node->right = right_rotate(node->right);
+        return left_rotate(node);
+    }
+
+    return node;
+}*/
+class UserName{
+public:
+    string name,Email,userName,FriendBST;
+    string getname(){
+        return name;
+    }
+    string getEmail(){
+    return Email;
+    }
+    string getuserName(){
+        return userName;
+    }
+    string getfriend(){
+        return FriendBST;
+    }
+
+};
 
 class List{
     public:
-    string name,Email,userName,FriendBST;
+    UserName user;
     List* next;
 
 };
-void login(List** head_ref,string userName,string Email,string name)
+void unique(List* node,string name){
+    while (node != NULL)
+    {
+        if(node->user.userName==name){
+            cout<<"the user name must be unique \n";
+            return;
+        }
+    }
+
+}
+void login(List** head_ref,UserName user)
 {
     List* new_node = new List();
 
     List *last = *head_ref;
-
-    new_node->name = name;
-    new_node->Email=Email;
-    new_node->userName=userName;
-
-
+    new_node->user=user;
+    unique(new_node,new_node->user.userName);
     new_node->next = NULL;
 
 
@@ -71,7 +143,7 @@ void printList(List *node)
 {
     while (node != NULL)
     {
-        cout<<" "<<node->name<<" "<<node->Email<<" "<<node->userName<<" "<<node->FriendBST<<endl;
+        cout<<" "<<node->user.getname()<<" "<<node->user.getEmail()<<" "<<node->user.getuserName()<<" "<<node->user.getfriend()<<endl;
         node = node->next;
     }
 }
@@ -80,6 +152,7 @@ int main()
 {
     List* head=NULL;
     cout<<"Without friend \n";
+    /*
     login(&head,"Soad Saber","soad91@gmail.com","soad91");
     login(&head,"Olaa Kamel","Olaa100@Yahoo.com","Olaa100");
     login(&head,"Emad Salem,","emadsalem@gmail.com","emadsalem1");
@@ -89,7 +162,23 @@ int main()
     login(&head,"emy2000","Iman Kareem","emy2000@gmail.com");
     login(&head,"talaat3000","Talaat Mahmoud","talaat3000@gmail.com");
     login(&head,"samir22","Samir Abdelshakoor","samir22@gmail.com");
+    */
+    UserName user;
+    user.name="ibrahim";
+    user.Email="Ibrahim@gmail.com";
+    user.userName="ibra0007";
+    user.FriendBST="mostafa";
+    UserName user2;
+    user2.name="mohamed";
+    user2.Email="mohamed@gmail.com";
+    user2.userName="mohamed 21";
+    user2.FriendBST="ihabe";
+    login(&head,user);
+    login(&head,user2);
     printList(head);
+    UserName user3;
+    user3.userName="ibrahim";
+    login(&head,user3);
     /**
     cout<<"With Friend \n";
     List* head1=NULL;
