3a0cee089e1ea07e79f8e58cf40eef70a8019d80
ibrahimtarek0072 <81970421+ibrahimtarek0072@users.noreply.github.com>
2021-07-14 09:06:30 -0700

    Create cmake_clean.cmake

---
+++ DS project final struct/cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/List.cpp.obj"
+  "CMakeFiles/untitled.dir/main.cpp.obj"
+  "CMakeFiles/untitled.dir/treap.cpp.obj"
+  "CMakeFiles/untitled.dir/users.cpp.obj"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
